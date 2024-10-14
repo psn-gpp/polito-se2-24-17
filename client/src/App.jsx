12c49d55923d7000a65647599a57e09a76a6dc14
Sefa Kurtoğlu <bmkurtoglusefa@gmail.com>
2024-10-14 20:11:12 +0200

    [FE] main components
    creation of main components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,24 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import DefaultRoute from './components/DefaultRoute';
 import HomeRoute from './components/HomeRoute';
+import CustomerDashboard from './components/CustomerDashboard';
+import ManagerDashboard from './components/ManagerDashboard';
+import OfficerDashboard from './components/OfficerDashboard';
+import NotFound from './components/NotFound';
+
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HomeRoute />} />
-          <Route path='/*' element={<DefaultRoute />} />
-        </Routes>
+      <Routes>
+        <Route path='/' element={<HomeRoute />} />
+        <Route path='/*' element={<DefaultRoute />} />
+        {/* Define routes for different user dashboards */}
+        <Route path='/customer' element={<CustomerDashboard />} />
+        <Route path='/manager' element={<ManagerDashboard />} />
+        <Route path='/officer' element={<OfficerDashboard />} />
+        {/* Handle undefined routes */}
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
   );
 }
